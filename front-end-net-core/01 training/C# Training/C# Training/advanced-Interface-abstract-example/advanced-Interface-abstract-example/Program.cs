8fdc77186747cbcb05558a88b6798f4b8f17562e
MohdNaqeem <144013508+MohdNaqeem@users.noreply.github.com>
2023-12-09 19:13:48 +0530

    first commit

---
+++ front-end-net-core/01 training/C# Training/C# Training/advanced-Interface-abstract-example/advanced-Interface-abstract-example/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AbstractClassDemo
+{
+    abstract class iPhone
+    {
+        //Non-Abstract Method   
+        public void Call()
+        {
+            Console.WriteLine("Call Method: This method provides Calling features");
+        }
+    }
+
+    class Program : iPhone
+    {
+        static void Main(string[] args)
+        {
+            //Instance Creation of Derived Class   
+            Program program = new Program();
+            program.Call();
+            Console.ReadKey();
+        }
+    }
+}
