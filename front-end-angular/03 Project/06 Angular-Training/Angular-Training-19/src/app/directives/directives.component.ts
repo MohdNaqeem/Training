cfb8895512b6dcbaafd1ca0bcfdc9aa93fef34b8
Naqeem Momin <mominnaqeem9860@gmail.com>
2025-06-30 17:26:08 +0530

    .nojekyll file added

--- front-end-angular/03 Project/06 Angular-Training/Angular-Training-19/src/app/directives/directives.component.ts
+++ front-end-angular/03 Project/06 Angular-Training/Angular-Training-19/src/app/directives/directives.component.ts
@@ -1,12 +1,43 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-directives',
   standalone: true,
-  imports: [],
+  imports: [NgFor, NgIf],
   templateUrl: './directives.component.html',
-  styleUrl: './directives.component.css'
+  styleUrl: './directives.component.css',
 })
 export class DirectivesComponent {
+  students = ['Momin', 'Naqeem', 'Raza', 'Mohammad'];
 
+  studentsData = [
+    {
+      name: 'Naqeem',
+      age: 22,
+      email: 'mominnaqeem@gmail.com',
+    },
+    {
+      name: 'Momin',
+      age: 21,
+      email: 'momin@gmail.com',
+    },
+    {
+      name: 'Raza',
+      age: 24,
+      email: 'Raza@gmail.com',
+    },
+    {
+      name: 'Khan',
+      age: 25,
+      email: 'Khan@gmail.com',
+    },
+    {
+      name: 'Mohammad',
+      age: 64,
+      email: 'Mohammad@gmail.com',
+    },
+  ];
+
+  condition = false
 }
