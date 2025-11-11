1f50acec35072b5bc436027156c0a2814c6309f2
Momin1234k <kparveen0129@gmail.com>
2025-11-11 19:09:51 +0530

    strings rivised

---
+++ front-end-javascript/19 strings/App.js
@@ -0,0 +1,18 @@
+// String:
+
+let str = "Hello world!"
+let data = 'Hello world!'
+
+console.log(str)
+console.log(data)
+
+// Template strings:
+
+let quotes = `String with 'single' andd "double" quotes`
+console.log(quotes)
+
+// Template literals
+
+let num = 20
+let val = 20
+console.log(`"This is num" ${num} "and its" ${val}`)
