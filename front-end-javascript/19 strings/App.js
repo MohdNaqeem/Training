64dbb3e783485dc894db9665e88bd47ac8195687
Momin1234k <kparveen0129@gmail.com>
2025-11-19 14:46:51 +0530

    javascript changes

--- front-end-javascript/19 strings/App.js
+++ front-end-javascript/19 strings/App.js
@@ -16,3 +16,14 @@ console.log(quotes)
 let num = 20
 let val = 20
 console.log(`"This is num" ${num} "and its" ${val}`)
+
+/* 
+Practice 
+
+01 Prompt the user to enter their full name. Generate a username for them based on the input. 
+Start username with @, followed by their full name and ending with the full name length.
+
+*/
+
+let userName = prompt("Enter your Full Nam without spacee")
+console.log(`@${userName}${userName.length}`)
---
+++ front-end-javascript/20 arrays/App.js
@@ -0,0 +1,14 @@
+// Array:
+
+const cars = ["Saab", "Volvo", "BMW"];
+console.log(cars)
+
+for(let car of cars){
+    console.log(`for of loop ${car}`)
+}
+
+for(i = 0; i<cars.length; i++){
+    console.log(`for loop ${cars[i]}`)
+    
+}
+
