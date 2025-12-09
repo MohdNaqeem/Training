9dbfd681a5b96ff385a7552077c00bb482c364b8
Momin1234k <kparveen0129@gmail.com>
2025-12-09 16:28:43 +0530

    React Episode 11

---
+++ front-end-react/11 Episode-Data-is-the-new-oil/src/utils/constants.js
@@ -0,0 +1,7 @@
+export const LOGO_URL =
+  "https://media.istockphoto.com/id/1138234161/vector/delivery-illustration.jpg?s=612x612&w=0&k=20&c=BXRhh9OYBOEbpfUD9DrO-aOLoyoqyzVj6s2EQyXQYRQ=";
+
+export const CDN_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+export const RES_CARD_URL =
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
