88b0083c51790d6397e6e85b73a666e83c445837
Momin1234k <kparveen0129@gmail.com>
2025-12-10 21:06:45 +0530

    javascript notes updated

--- front-end-javascript/05 shortest-js-program-window-&-this-keyword/App.js
+++ front-end-javascript/05 shortest-js-program-window-&-this-keyword/App.js
@@ -6,13 +6,3 @@ function b(){
 
 console.log(x)
 console.log(this.a)
-
-/*
-Notes :
-
-- Shortest Program in JS: Empty file. Still, browsers make global EC and global space along with Window 
-  object.
-  
-- Global space mean anything which is not inside function that is store in the global store. 
-
-*/
--- front-end-javascript/06 undefined-vs-not-defined-in-js/App.js
+++ front-end-javascript/06 undefined-vs-not-defined-in-js/App.js
@@ -2,21 +2,3 @@ var a = 10;
 console.log(a)
 
 console.log(x)
-
-/*
-Notes :
-
-- Undefined is a Special Placeholder which is used to reserve memory for the variables in the memory 
-  creation phase. Even before a single line of code is executed JS engine assigns undefined to the 
-  variables.
-
-- Not Defined means if we try to console or access any variable which is not declared in the code 
-  then we get Not Defined error.
-
-- JS is a loosely typed language or weakly typed language means it does not attaches its variables 
-  to specific data types.
-
-- Most impoertant this undefined !== not defined 
-  undefined mean which is declared but value is not read and not defined mean which is not 
-  decalred.
-*/
